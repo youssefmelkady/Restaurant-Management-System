48b835121ca075b7da57fe744e9a7779c9b71f24
youssef <youssefelkady2003@gmail.com>
2026-08-17 23:10:25 +0300

    CustomerAccount final and updated version (comments)

--- RestaurantSystem/src/com/restaurant/billing/CustomerAccount.java
+++ RestaurantSystem/src/com/restaurant/billing/CustomerAccount.java
@@ -2,33 +2,44 @@ package com.restaurant.billing;
 
 public class CustomerAccount {
 
+    //data field
     private double balance;
     private int loyaltyPoints;
 
+    //constructor(with parameters)
     public CustomerAccount(double balance, int loyaltyPoints) {
 
+        //invalid starting exception(balance)
         if (balance < 0) {
             throw new IllegalArgumentException(
                     "Balance cannot be negative"
             );
         }
 
+        //invalid starting exception(loyalty Points)
         if (loyaltyPoints < 0) {
             throw new IllegalArgumentException(
                     "Loyalty points cannot be negative"
             );
         }
 
+        //assign
         this.balance = balance;
         this.loyaltyPoints = loyaltyPoints;
     }
 
+    //getters
     public double getBalance() {
         return balance;
     }
+    public int getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
 
+    //methods
     public boolean deductBalance(double amount) {
 
+        //checking balance
         if (amount <= 0) {
             return false;
         }
@@ -41,12 +52,11 @@ public class CustomerAccount {
         return true;
     }
 
-    public int getLoyaltyPoints() {
-        return loyaltyPoints;
-    }
+
 
     public boolean deductLoyaltyPoints(int points) {
 
+        //checking points
         if (points <= 0) {
             return false;
         }
